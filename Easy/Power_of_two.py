eff2cd8787edcda94596fe19eb7063eae91af39f
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-08-12 17:53:37 +0530

    Create Power_of_two.py

---
+++ Easy/Power_of_two.py
@@ -0,0 +1,8 @@
+from math import sqrt
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n==1:
+            return True
+        elif n%2 != 0 or n == 0:
+            return False
+        return self.isPowerOfTwo(n//2)               
