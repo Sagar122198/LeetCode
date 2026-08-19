3d9b6703741b605bc94da7a28200c0ca724fdda6
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-08-19 17:21:25 +0530

    Create Sort _an_Array.py

---
+++ Medium/Sort _an_Array.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def sortArray(self, nums):
+        def merge_arr(left,right):
+            result = []
+            i,j=0,0
+            n,m=len(left), len(right)
+            while i<n and j<m:
+                if left[i]<=right[j]:
+                    result.append(left[i])
+                    i+=1
+                else:
+                    result.append(right[j])
+                    j+=1
+            while i<n:
+                result.append(left[i])
+                i+=1
+            while j<m:
+                result.append(right[j])
+                j+=1
+            return result
+        
+        if len(nums)==1:
+                return nums
+        mid = len(nums)//2
+        left_arr =nums[:mid]
+        right_arr =nums[mid:]
+        left = self.sortArray(left_arr)
+        right = self.sortArray(right_arr)
+        return merge_arr(left,right)
