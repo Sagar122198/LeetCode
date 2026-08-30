784b6202d8f447eda2e003bec134af535fd46d47
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-08-30 13:05:24 +0530

    Create Move_Zeroes_Optimal_Solution.py

---
+++ Easy/Move_Zeroes_Optimal_Solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        n = len(nums)
+        if n == 1:
+            return nums
+        i = 0
+        while i<n:
+            if nums[i] ==0:
+                break
+            i +=1
+        if i == n:
+            return nums
+        j = i+1
+        while j <n:
+            if nums[j] != 0:
+                nums[i],nums[j] = nums[j] , nums[i]
+                i+=1
+            j+=1
