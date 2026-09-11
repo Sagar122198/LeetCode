fc1b422c6883fb1c5fa8c57307a2ba994677dac0
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-09-11 16:59:56 +0530

    Implement longest consecutive sequence function

---
+++ Medium/Longest_Consecutive_Sequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        n = len(nums)
+        my_set = set()
+        for i in range(0,n):
+            my_set.add(nums[i])
+        longest = 0
+        for num in my_set:
+            if num-1 not in my_set:
+                count = 1
+                x = num
+                while x+1 in my_set:
+                    count +=1
+                    x+=1
+                longest = max(longest,count)
+        return longest
