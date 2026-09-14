57820e50e308005ab470bee6e341e589e5473814
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-09-14 17:00:25 +0530

    Implement brute force solution to rotate image

---
+++ Medium/Rotate_Image_Brute_Force_Solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+        result = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(0,n):
+            for j in range(0,n):
+                result[j][(n-1)-i] = matrix[i][j]
+        matrix[:] = result
